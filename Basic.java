0b364cd8c9c51f4f9f35fb7bbe4c0feeced943cd
Sowmiya <113198036+sowmiya1320@users.noreply.github.com>
2023-02-23 11:31:59 +0530

    Update Basic.java

--- Basic.java
+++ Basic.java
@@ -4,7 +4,6 @@ public class Basic {
 
 	public static void main(String[] args) {
 		String txt="abcdefghijklmnopqrstuvwxyz";
-		String str="Sowmiya's crush is \"namjoon\" from bts";
 		System.out.println("The String name txt having "+txt.length()+"character");
 		System.out.println(txt.toUpperCase());
 		System.out.println(str);
